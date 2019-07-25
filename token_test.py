76115601c0f987fe3be9e49ecc8e222886e890b8
Kyle Snow <kjsnow11@gmail.com>
2019-07-24 22:21:54 -0400

    token_test: reading playlist tracks and other methods

--- token_test.py
+++ token_test.py
@@ -11,7 +11,7 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 USERNAME = 'kjsnow'
-SCOPE = 'user-library-modify, playlist-read-private'
+SCOPE = 'user-library-modify, playlist-read-private, user-library-read'
 CLIENT_ID = '2d0cc7a4ed9d44a69c9ad358b216dd7e'
 CLIENT_SECRET = 'bc85301ff7114dca9f2a195804b16ddc'
 REDIRECT_URI = 'http://localhost:8000'
@@ -43,10 +43,44 @@ new_music = new_music[0]
 
 username='kjsnow'
 
-# Get dict of all user playlists
-playlists = sp.user_playlists(username)
-def get_playlist(playlists, title):
-    return playlists['items'][next((index for (index, d) in enumerate(playlists['items']) if d['name']== title), None)]
+def get_user_playlist(username, playlist_name):
+    # Get dict of all user playlists
+    playlists = sp.user_playlists(username)
+    return playlists['items'][next((index for (index, d) in enumerate(playlists['items']) if d['name'] == playlist_name), None)]
 
 #
-my_playlist = get_playlist(playlists, 'New Music Friday')
+my_playlist = get_user_playlist(username, 'New Music Friday')
+
+
+my_playlist_tracks = sp.user_playlist_tracks(username, playlist_id=my_playlist['id'])
+
+
+
+
+# GET USER SAVED TRACKS
+import sys
+import spotipy
+import spotipy.util as util
+
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print ("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username = USERNAME,
+                                   scope = SCOPE,
+                                   client_id = CLIENT_ID,
+                                   client_secret = CLIENT_SECRET,
+                                   redirect_uri = REDIRECT_URI)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks(limit=50)
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
